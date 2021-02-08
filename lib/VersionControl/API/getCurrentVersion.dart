b854208672ab00d5b0e0456a4e2b33149160a28f
aytoku <seturidee@mail.ru>
2021-02-08 14:46:13 +0300

    lala

--- lib/VersionControl/API/getCurrentVersion.dart
+++ lib/VersionControl/API/getCurrentVersion.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 Future<CurrentVersionModel> getCurrentVersion() async {
-  await CreateOrder.sendRefreshToken();
   CurrentVersionModel currentVersion = null;
   var url = 'http://78.110.156.74:3003/api/v3/versions/last';
   String token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMTIzIiwibmFtZSI6InRlc3RfdXNlciIsImV4cCI6ODgxMTEzODMzMywiaWF0IjoxNjExMTM4MzMzLCJpc3MiOiJhdXRoX3NlcnZpY2UifQ.A3hMQOcQFqZ19bj_W86EXmIPpV9jnlZjK6PACARXLiXo6D0Lc2axLDx-uRIu2aEqjtdGdOLL_wv0ICV9oYXIUgLy8jHQtMrHNEHYDzJJSTjc0yF0wXGIT5LtjpeXcA6-ZxyxwpmjeFBpf3lTPDCEWTeRlY-2EMTSgU4C5J_WkcsalOpgwKAtOJGHRPCwq4AZ2jhPTd1ZwR1GaMY08tDVIHWMRi6IQih12RBg1dixkhJms5U10mX_uzIixhMSoQuH1NkPPeujkjGJCOEBD5NG7XP06o2kFCp7TMAnexwlh5FQWHZluK8snkUL9QMzppCZvUDWDd-fCweKyyBw9-Emqw';
