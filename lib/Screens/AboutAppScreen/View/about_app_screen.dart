3b043ab043d6a268121b0ce37e015a97b33e2f99
aytoku <seturidee@mail.ru>
2021-03-09 13:54:47 +0300

    lala

--- lib/Screens/AboutAppScreen/View/about_app_screen.dart
+++ lib/Screens/AboutAppScreen/View/about_app_screen.dart
@@ -41,7 +41,7 @@ class AboutAppScreenState extends State<AboutAppScreen> {
                           padding: EdgeInsets.only(bottom: 25),
                           child: Center(
                             child: Text(
-                              'Версия 1.0.10 от 6 мар. 2021 г.\nсборка 18',
+                              'Версия 1.0.10 от 9 мар. 2021 г.\nсборка 18',
                               textAlign: TextAlign.center,
                               style: TextStyle(color: Color(0x97979797), fontSize: 15),
                             ),
